9712f7bef14b4deea06aaf22cd7f92b3391752da
egetman <getman.eugene@gmail.com>
2018-10-15 17:30:27 +0300

    Add readme.md with some thoughts about implementation

---
+++ readme.md
@@ -0,0 +1,9 @@
+Think about:
+ - version caching? to avoid every-write check
+ - multithreaded write - to be or not to be?
+ - ~~store drain-to? - recreate event store~~ DONE
+ - event-intersepter ?? handling?
+ - make snapshotting
+ - store structure validation on start
+ - event idempotency on read (clustered environment)
+ - event steam deletion
--- src/test/java/io/jes/provider/JdbcStoreProviderTest.java
+++ src/test/java/io/jes/provider/JdbcStoreProviderTest.java
@@ -4,14 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import static io.jes.provider.FancyStuff.newDataSource;
 
-// todo: version caching? to avoid every-write check
-// todo: multithreaded write - to be or not to be?
-// todo: store drain-to? - recreate event store
-// todo: event-intersepter ?? handling?
-// todo: make snapshotting
-// todo: store structure validation on start
-// todo: event idempotency on read (clustered environment)
-
 
 @Slf4j
 class JdbcStoreProviderTest extends StoreProviderTest {
